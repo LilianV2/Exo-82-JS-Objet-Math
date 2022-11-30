f37947004f3fea6c7f8091a332ba90b7e3a09463
lilia <lilianom59@outlook.fr>
2022-11-30 19:27:51 +0100

    math

---
+++ exo.js
@@ -0,0 +1,50 @@
+let nb1 = 34.589456;
+nb1 = Math.floor(nb1)
+console.log('1 => ' + nb1)
+
+let nb2 = 68.412784;
+nb2 = Math.floor(nb2)
+console.log('2 => ' + nb2)
+
+let nb3 = 39.456;
+nb3 = Math.ceil(nb3)
+console.log('3 => ' + nb3)
+
+let nb4 = 27.34876541;
+nb4 = Math.ceil(nb4)
+console.log('4 => ' + nb4)
+
+let nb5 = 5.68;
+nb5 = Math.round(nb5)
+console.log('5 => ' + nb5)
+
+let nb6 = 5.41;
+nb6 = Math.ceil(nb6)
+console.log('6 => ' + nb6)
+
+let nb7 = 6.12457;
+nb7 = Math.ceil(nb7)
+console.log('7 => ' + nb7)
+
+let nb8 = 9.568124;
+nb8 = Math.ceil(nb8)
+console.log('8 => ' + nb8)
+
+let table1 = Math.min(5, 1, 12, -1, 45);
+console.log( '9 => ' + table1);
+
+let table2 = Math.min(9, 12, 16, 44, 3);
+console.log( '10 => ' + table2);
+
+let table3 = Math.max(5, 1, 12, -1, 45);
+console.log( '11 => ' + table3);
+
+let table4 = Math.max(9, 12, 16, 44, 3);
+console.log( '12 => ' + table4);
+
+let nb13 = Math.abs(-6)
+console.log('13 => ' + nb13)
+
+let nb14 = Math.abs(-56)
+console.log('14 => ' + nb14)
+
